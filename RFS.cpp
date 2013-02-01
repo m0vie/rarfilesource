1878679f79c4af5f91f22a9d7bad190e8bc648e5
m0viefreak <m0viefreak.cm@googlemail.com>
2013-02-01 17:59:23 +0100

    improve error messages to reflect compressed and encrypted files
    - Move "no files found" error message into ScanArchive().
      This avoids a redundant error message, if ScanArchive() was ended
      early, because of a damaged / completely encrypted / ... archive.
    - Keep track of the number of encrypted and compressed files, and,
      if no usable media file was found, show error messages to indicate
      thse compressed or unusable files, if present. In this case the
      encrypted files message gets a higher priority than the compressed
      message.

--- RFS.cpp
+++ RFS.cpp
@@ -272,6 +272,8 @@ int CRARFileSource::ScanArchive (wchar_t *archive_name, List<File> *file_list, i
 	File *file = NULL;
 
 	*ok_files_found = 0;
+	int compressed_files_found = 0;
+	int encrypted_files_found = 0;
 	LARGE_INTEGER zero = {0};
 
 	MediaType *mType;
@@ -505,12 +507,14 @@ int CRARFileSource::ScanArchive (wchar_t *archive_name, List<File> *file_list, i
 
 				if (rh.ch.flags & LHD_PASSWORD)
 				{
+					encrypted_files_found++;
 					DbgLog((LOG_TRACE, 2, L"Encrypted files are not supported."));
 					file->unsupported = true;
 				}
 
 				if (rh.fh.method != 0x30)
 				{
+					compressed_files_found++;
 					DbgLog((LOG_TRACE, 2, L"Compressed files are not supported."));
 					file->unsupported = true;
 				}
@@ -672,6 +676,22 @@ int CRARFileSource::ScanArchive (wchar_t *archive_name, List<File> *file_list, i
 		ErrorMsg (0, L"Couldn't find all archive volumes.");
 	}
 
+	// only show error messages if no usable file was found
+	if (*ok_files_found == 0)
+	{
+		if (encrypted_files_found > 0)
+		{
+			ErrorMsg (0, L"Encrypted files are not supported.");
+		}
+		else if (compressed_files_found > 0)
+		{
+			ErrorMsg (0, L"Compressed files are not supported.");
+		}
+		else {
+			ErrorMsg (0, L"No media files found in the archive.");
+		}
+	}
+
 	return files;
 }
 
@@ -775,7 +795,6 @@ STDMETHODIMP CRARFileSource::Load (LPCOLESTR lpwszFileName, const AM_MEDIA_TYPE 
 
 	if (!num_ok_files)
 	{
-		ErrorMsg (0, L"No media files found in the archive.");
 		return E_UNEXPECTED; // TODO: Figure out a better error code.
 	}
 
